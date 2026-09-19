f50b63619f795e4a374a3ed92630cb671303cfe0
priyanshu thakur <priyqnshu94@gmail.com>
2026-09-19 08:55:23 +0530

    problem no.110 solved

---
+++ Trees/110.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+int height(TreeNode* root){
+    if(root==NULL) return 0;
+    int left=height(root->left);
+    int right=height(root->right);
+    return 1+max(left,right);
+}
+
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL)return true;
+       int leftheight=height(root->left);
+       int rightheight=height(root->right);
+       if(abs(leftheight-rightheight)>1) return false;
+       if(!isBalanced(root->left)) return false;
+       if(!isBalanced(root->right)) return false;
+       return true;
+    }
+
+};
