215b952b4706b096d3e572bd37355757f89a42fe
priyanshu thakur <priyqnshu94@gmail.com>
2026-09-23 12:00:40 +0530

    problem no.124 solved

---
+++ Trees/124.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+int maxpath(TreeNode* root,int &maxi){
+    if(root==NULL) return 0;
+    int left=max(0,maxpath(root->left,maxi));
+    int right=max(0,maxpath(root->right,maxi));
+    maxi=max(maxi,root->val+left+right);
+    return root->val+max(left,right);
+}
+
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxi=INT_MIN;
+     
+     maxpath(root,maxi);
+     return maxi;
+    }
+};
